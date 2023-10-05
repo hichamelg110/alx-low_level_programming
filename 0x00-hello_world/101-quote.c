14521abe085f0c09f1ed7d94e5e1f4883ed58c8f
hichamelg110 <elisejuneaileen@gmail.com>
2023-10-05 08:50:18 -0700

    task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program.
+ * a C program that prints exactly and that piece
+ * of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+fprintf(stdout, "and that piece of art is useful\" - Dora ");
+fprintf(stdout, "Korpar, 2015-10-19\n");
+
+return (1);
