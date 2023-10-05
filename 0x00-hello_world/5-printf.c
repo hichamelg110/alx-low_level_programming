acdac47fd5f3cf776e0cc4aeb6bc0188ac7fcdf3
hichamelg110 <elisejuneaileen@gmail.com>
2023-10-05 06:35:12 -0700

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program.
+ * a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+		return (0);
+}
