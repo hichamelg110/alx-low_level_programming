8be069233f8714082e241fcfebbb0e66c548e805
hichamelg110 <elisejuneaileen@gmail.com>
2023-10-10 13:47:02 -0700

    task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - a program that prints _putchar
+ * followed by a new line.
+ * Return: 0.
+ */
+
+void main(){
+    
+for (int i = 'a'; i <= 'z'; i++)
+
+{ 
+putchar(i);
+}
+putchar('\n');
+if (islower(c))
+return (1);
+ 
+else
+return (0);
+}
