80c124551a72d0336ac86d86150c681510d8a945
hichamelg110 <elisejuneaileen@gmail.com>
2023-10-10 03:35:57 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar
+ * followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+
+return (0);
+}
+
