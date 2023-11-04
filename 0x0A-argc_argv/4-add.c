2265b1b683298ef7ebb1da6a5a3a866910b4da3c
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-03 17:35:52 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,7 +8,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 int i, sum = 0;
 
 if (argc == 1) 
@@ -16,9 +17,12 @@ if (argc == 1)
 printf("0\n");
 return (0);
 }
-for (i = 1; i < argc; i++) {
-for (int j = 0; argv[i][j] != '\0'; j++) {
-if (!isdigit(argv[i][j])) {
+for (i = 1; i < argc; i++)
+{
+for (int j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
 printf("Error\n");
 return (1);
 }
@@ -29,4 +33,3 @@ sum += atoi(argv[i]);
 printf("%d\n", sum);
 return (0);
 }
-
