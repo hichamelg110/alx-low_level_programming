0832d52207f1f76f40a6a07f923b376c5661bb0d
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-06 06:30:41 -0800

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-[2;2R[>77;30604;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000/**
- * struct dog - Define a new type struct dog with the following elements
+/**
+ * struct dog - Define a new type struct dog
  * @name: the name
  * @age: the age
  * @owner: the owner
@@ -15,6 +15,7 @@ char *owner;
 };
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
