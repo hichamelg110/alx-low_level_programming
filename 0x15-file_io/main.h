7346dede691afe7cee9fb7dbbb0e01a6d8523f58
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-27 14:32:23 -0800

    task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
