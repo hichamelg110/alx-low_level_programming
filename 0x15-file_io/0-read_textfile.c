ed945fcfe28c3739495c7f9a6a408cae35927f1d
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-27 15:33:26 -0800

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdblib.h> 
+#include <stdblib.h>
 /**
  * read_textfile - Read text file and print to STDOUT.
  * @filename: Pointer to the name of the file
@@ -9,23 +9,19 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t c;
-int fd;
-char buf[READ_BUF8SIZE * 8];
+char *buffer;
+ssize_t fd;
+ssize_t write_b;
+ssize_t read_b;
 
-if (filename == NULL || letters == 0)
-{
-return (0);
-}
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
 return (0);
-}
-c = read(fd, buf, letters);
-c = write(STDOUT_FILENO, buf, c);
+buffer = malloc(sizeof(char) * letters);
+read_b = read(fd, buffer, letters);
+write_b = write(STDOUT_FILENO, buffer, read_b);
+
+free(buffer);
 close(fd);
-
-return (c);
+return (write_b);
 }
-
