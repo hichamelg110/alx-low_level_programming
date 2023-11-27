a64199a273bb9fb4fa1f99b08f531b8298541b40
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-27 13:55:16 -0800

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,19 +12,19 @@ ssize_t c;
 int fd;
 char buf[READ_BUF8SIZE * 8];
 
-if (filename == NULL || letters == NULL)
+if (filename == NULL || letters == 0)
 {
-return(0);
+return (0);
 }
-fd = open(filename, O_RDONLY):
-
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
 return (0);
 }
-c = read(fd, &buf[0]. letters);
-c = write(STDOUT_FILENO. &buf[0]. c);
+c = read(fd, buf, letters);
+c = write(STDOUT_FILENO, buf, c);
 close(fd);
 
 return (c);
 }
+
